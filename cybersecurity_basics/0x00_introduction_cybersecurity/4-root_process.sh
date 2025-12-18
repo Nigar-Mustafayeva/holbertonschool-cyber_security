15fdaeed36786c057facac18cae1fdf378c7df9f
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-12-18 11:16:07 +0000

    .

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ps aux | grep -v "VSZ|RSS" "$1"  
+
+# Check if username was provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+# Monitor processes of the user, excluding VSZ or RSS equal to 0
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd --no-headers | grep -v -E '\s0\s0\s'
