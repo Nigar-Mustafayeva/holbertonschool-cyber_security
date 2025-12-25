7ae7233af72fdfa0b6908c4017e48e499c519611
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-12-25 09:25:11 +0000

    .

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '$1=="Registant"' > who is "$1"; awk '$2=="Admin"' > whois "$1" ; awk '$3=="Tech"' > whois "$1" 
+awk '$1=="Registrant"' > who is "$1"; awk '$2=="Admin"' > whois "$1" ; awk '$3=="Tech"' > whois "$1" 
