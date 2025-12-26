278f0876414d2836519bba0d448ad91347cfef57
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-12-26 11:37:02 +0000

    .

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext|Email):/{f=$1; v=$2; if(f~/Street$/) v=v" "; if(f~/Ext$/) f=f":"; sub(/:$/,"",f); printf "%s,%s\n", f, v}'
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
