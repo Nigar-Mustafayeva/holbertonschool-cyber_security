f0c0641fa58f33b5f558d949147863cc4440b525
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-01-18 11:54:01 +0000

    jdhh

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1" | sed 's/{xor}//' | base64 -d | xxd -p | sed 's/../0x& /g' | awk '{for(i=1;i<=NF;i++) printf "%c", strtonum($i)^42} END{print ""}'
+echo "$1" | sed 's/{xor}//' | base64 -d | od -An -tu1 | tr -s ' ' '\n' | while read n; do printf "\\$(printf '%03o' $((n^42)))"; done
