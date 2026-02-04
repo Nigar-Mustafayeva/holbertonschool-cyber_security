705b11e436f755726bf21ac95ddf16063dfe280b
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-02-04 05:44:58 -0500

    .

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,1 +1,2 @@
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=nt "$1" | cut -d: -f2 > 5-password.txt
+#!/bin/bash
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" | john --show --format=nt "$1" | cut -d ":" -f2 > 5-password.txt
