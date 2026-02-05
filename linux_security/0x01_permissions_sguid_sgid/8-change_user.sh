ec749619ed536b665c7854ad701569e678dd9434
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-02-05 05:34:40 -0500

    .

--- linux_security/0x01_permissions_sguid_sgid/8-change_user.sh
+++ linux_security/0x01_permissions_sguid_sgid/8-change_user.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find "$1" -perm 2000 -o 4000 -type f 2>/dev/null -exec ls -l {} \; | sudo chown -R user3 "$1"
+find "$1" -perm 2000 -o 4000 -type f 2>/dev/null -exec ls -l {} \; | if [ $(find user == "user2")] then sudo chown -R user3 "$1" fi
