9c193b4dfddaa1d67f4f3738db6e7495eebce3b7
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-02-10 03:47:29 -0500

    .

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "^(Port|PermitRootLogin|PasswordAuthentication|AllowUsers|AllowGroups)" /etc/ssh/sshd_config; sudo ss -tulpn | grep ssh
+grep -Ev "^(Port|PermitRootLogin|PasswordAuthentication|AllowUsers|AllowGroups)" /etc/ssh/sshd_config; sudo ss -tulpn | grep ssh
