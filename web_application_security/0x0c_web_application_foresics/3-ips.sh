5ff50172d044dd282901984abf359984885206d5
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-04-17 02:23:09 -0400

    .

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-if [ -z "$1" ]
-then
-    echo "Usage: $0 auth.log"
-    exit 1
-fi
-
-grep "Accepted" "$1" | awk '{print $11}' | sort -u | wc -l
+grep "Accepted" $1 | awk '{print $11}' | sort -u | wc -l
