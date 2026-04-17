d3588d333a4f29a9c8b40747ac46f8428ff78b65
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-04-17 01:43:27 -0400

    .

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep -E "Failed password|Accepted password" auth.log | awk '{for(i=1;i<=NF;i++) if($i=="for"){if($(i+1)=="invalid") print $(i+3); else print $(i+1)}}' | sort -u | paste -sd,
+grep "sshd" auth.log | grep -E "Accepted password|Failed password" | awk '{for(i=1;i<=NF;i++) if($i=="for"){if($(i+1)!="invalid") print $(i+1)}}' | sort -u | paste -sd,
