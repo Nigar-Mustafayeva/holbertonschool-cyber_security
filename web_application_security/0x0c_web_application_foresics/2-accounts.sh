c4d81d2587459644a4cfbdaf9c1a138789f3dbaa
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-04-17 01:22:59 -0400

    .

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-tail -1000 auth.log | grep -E "Failed password|Accepted password" | awk '{print $(NF)}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+tail -1000 auth.log | grep -E "Failed password for|Accepted password for" | awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
