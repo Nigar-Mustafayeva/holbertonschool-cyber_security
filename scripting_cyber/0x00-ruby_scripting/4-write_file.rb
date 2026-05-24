77ca414bbdccb3c778f5d79688beb1dddf3565c9
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-05-24 07:08:57 -0400

    .

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,10 @@
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+  file1_data = JSON.parse(File.read(file1_path))
+  file2_data = JSON.parse(File.read(file2_path))
+
+  merged = file2_data + file1_data
+
+  File.write(file2_path, JSON.generate(merged))
+end
